20a7b7d149417aa398bfb8bcc31a1588cd2fe2ae
baraaAlosaily <seelbaraa@gmail.com>
2021-04-20 04:37:37 +0300

    Editing of assignment class 06 constructor

--- app.js
+++ app.js
@@ -111,3 +111,7 @@ function getRandomIntInclusive(min,max){
   max=Math.floor(max);
   return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+// eslint-disable-next-line no-unused-vars
+let y=1;
+console.log(y);
